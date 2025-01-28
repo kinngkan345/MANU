f2002f5fb3732bc6cdf2ff2f5145934eaa98a176
kinngkan345 <ladlaking238438@gmail.com>
2025-01-29 01:07:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "put your github usearname",
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "put your github auth token without ghp_",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&JX9XVIRS#KILZ3WKGAIjYOb-A8QySeRHIokOgLzI-TILewTMtFPY",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "kinngkan345",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "mXKxhcrsH90uxjJFAQCm3L705rbBHX3NMHLL",
 };
